4e3c9690baf604a590ecc4eadcddd59998453d3e
Ashish Raghuvanshi <thetypo36@gmail.com>
2024-02-07 21:38:09 +0530

    deploy2

--- to-do-list-client/src/api/Axios.js
+++ to-do-list-client/src/api/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/",
+  baseURL: "https://todolistbackend-dszg.onrender.com  ",
 });
 
 export default instance;
